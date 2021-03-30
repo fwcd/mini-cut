f027a631e4b0ff5000309f2fc0b040156001a7f4
fwcd <fwcdmail@gmail.com>
2021-03-31 01:51:55 +0200

    Add another initializer for non-textual buttons

--- MiniCut.playground/Sources/View/Button.swift
+++ MiniCut.playground/Sources/View/Button.swift
@@ -1,11 +1,23 @@
 import Foundation
 import SpriteKit
 
+/// A simple UI control that displays a label and performs an action when clicked.
 public class Button: SKSpriteNode {
-    public convenience init(_ text: String, padding: CGFloat = 10) {
-        let label = SKLabelNode(text: text)
-        label.position = CGPoint(x: 0, y: -label.frame.size.height / 2)
+    public convenience init(label: SKNode, padding: CGFloat = ViewDefaults.padding) {
         self.init(color: NSColor(white: 1, alpha: 0.2), size: CGSize(width: label.frame.width + padding, height: label.frame.height + padding))
         addChild(label)
+    }
+    
+    /// Creates a textual button.
+    public convenience init(
+        _ text: String,
+        fontSize: CGFloat = ViewDefaults.fontSize,
+        fontName: String = ViewDefaults.fontName
+    ) {
+        let label = SKLabelNode(text: text)
+        label.fontSize = fontSize
+        label.fontName = fontName
+        label.position = CGPoint(x: 0, y: -label.frame.size.height / 2)
+        self.init(label: label)
     }
 }
---
+++ MiniCut.playground/Sources/View/ViewDefaults.swift
@@ -0,0 +1,8 @@
+import SpriteKit
+
+/// Default configurations for UI elements.
+public enum ViewDefaults {
+    public static let fontName: String = "Helvetica"
+    public static let fontSize: CGFloat = 24
+    public static let padding: CGFloat = 10
+}

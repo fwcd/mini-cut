f4f95180976736c09cf5ffa844348b4ded04b9c8
fwcd <fwcdmail@gmail.com>
2021-03-31 01:32:08 +0200

    Widen the scene view

--- MiniCut.playground/Contents.swift
+++ MiniCut.playground/Contents.swift
@@ -3,8 +3,8 @@
 import PlaygroundSupport
 import SpriteKit
 
-let width = 640
-let height = 480
+let width = 800
+let height = 500
 
 // Create and initialize the scene
 let sceneView = SKView(frame: CGRect(x: 0, y: 0, width: width, height: height))

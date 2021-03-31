3efa2e9c1286ac0ad1259b45a0da248fde807ba2
fwcd <fwcdmail@gmail.com>
2021-04-01 00:42:40 +0200

    Make video part of crop node

--- MiniCut.playground/Sources/View/VideoView.swift
+++ MiniCut.playground/Sources/View/VideoView.swift
@@ -3,21 +3,22 @@ import SpriteKit
 
 /// A view of the composited video.
 final class VideoView: SKNode {
+    private var size: CGSize!
     private var crop: SKCropNode!
     
     convenience init(size: CGSize) {
         self.init()
+        self.size = size
         
-//        crop = SKCropNode()
-//        crop.maskNode = SKSpriteNode(color: .white, size: size)
-//        addChild(crop)
+        crop = SKCropNode()
+        crop.maskNode = SKSpriteNode(color: .white, size: size)
+        addChild(crop)
         
         // TODO: Remove this sample and implement an actual composited video preview
         let video = SKVideoNode(fileNamed: "bigBuckBunny.mp4")
         video.size = size
-        video.position = CGPoint(x: 0, y: 0)
         video.zPosition = 100
-        addChild(video)
+        crop.addChild(video)
         video.play()
     }
 }

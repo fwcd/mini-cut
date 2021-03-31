686457c9a5b94daaf542a488939a9a4610ba936a
fwcd <fwcdmail@gmail.com>
2021-04-01 01:04:54 +0200

    Add ListenerList.silencing

--- MiniCut.playground/Sources/Utils/ListenerList.swift
+++ MiniCut.playground/Sources/Utils/ListenerList.swift
@@ -3,19 +3,27 @@ import Foundation
 /// A facility for pub-sub-style communication.
 class ListenerList<Event> {
     private var listeners: [UUID: (Event) -> Void] = [:]
+    private var silenced = Set<UUID>()
     
     /// Subscribes a listener to this list. Make sure to store the Subscription
     /// somewhere as the listener will be removed once the Subscription is dropped.
     func subscribe(_ listener: @escaping (Event) -> Void) -> Subscription {
         let id = UUID()
         listeners[id] = listener
-        return Subscription { [weak self] in
+        return Subscription(id: id) { [weak self] in
             self?.listeners[id] = nil
         }
     }
     
+    /// Performs an action without notifying the listener from the given subscription.
+    func silencing(_ subscription: Subscription, _ action: () -> Void) {
+        silenced.insert(subscription.id)
+        action()
+        silenced.remove(subscription.id)
+    }
+    
     func fire(_ event: Event) {
-        for listener in listeners.values {
+        for (id, listener) in listeners where !silenced.contains(id) {
             listener(event)
         }
     }
--- MiniCut.playground/Sources/Utils/Subscription.swift
+++ MiniCut.playground/Sources/Utils/Subscription.swift
@@ -1,8 +1,13 @@
+import Foundation
+
 /// An opaque object that performs cleanup after being dropped.
 class Subscription {
+    let id: UUID
+    
     private let handleEnd: () -> Void
     
-    init(handleEnd: @escaping () -> Void) {
+    init(id: UUID, handleEnd: @escaping () -> Void) {
+        self.id = id
         self.handleEnd = handleEnd
     }
     

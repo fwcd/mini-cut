c6b120d568afb33feda46807b41aba8a4da23e0c
fwcd <fwcdmail@gmail.com>
2021-04-01 00:56:38 +0200

    Initialize audio session in app

--- MiniCut/MiniCut/AppDelegate.swift
+++ MiniCut/MiniCut/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+        } catch {
+            fatalError("Could not initialize audio session")
+        }
         return true
     }
 

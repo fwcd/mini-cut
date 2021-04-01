65b13cd839249fa989d64b9ba81d19215a6baf89
fwcd <fwcdmail@gmail.com>
2021-04-01 22:20:54 +0200

    Let TrackClipView listen to state

--- MiniCut.playground/Sources/View/TrackClipView.swift
+++ MiniCut.playground/Sources/View/TrackClipView.swift
@@ -3,6 +3,8 @@ import SpriteKit
 
 /// A visual representation of a track's controls.
 final class TrackClipView: SKSpriteNode {
+    private var clipSubscription: Subscription!
+    
     convenience init(
         state: MiniCutState,
         trackId: UUID,
@@ -11,19 +13,26 @@ final class TrackClipView: SKSpriteNode {
         toViewScale: AnyBijection<TimeInterval, CGFloat>,
         toClipX: AnyBijection<TimeInterval, CGFloat>
     ) {
-        // TODO: Subscribe to listeners
-        guard let clip = state.timeline[trackId]?[id] else {
-            fatalError("Could not find track with id \(trackId)")
+        self.init()
+        
+        var hasGeneratedThumb = false
+        
+        clipSubscription = state.timelineDidChange.subscribeFiring(state.timeline) { [unowned self] in
+            guard let clip = $0[trackId]?[id] else { return }
+            
+            color = clip.clip.content.color
+            size = CGSize(width: toViewScale.apply(clip.clip.length), height: height)
+            
+            centerLeftPosition = CGPoint(x: toClipX.apply(clip.offset), y: 0)
+            
+            if !hasGeneratedThumb {
+                hasGeneratedThumb = true
+                let aspectRatio: CGFloat = 16 / 9
+                let thumbSize = CGSize(width: aspectRatio * height, height: height)
+                let thumb = generateThumbnail(from: clip.clip, size: thumbSize)
+                thumb.centerLeftPosition = CGPoint(x: frame.minX, y: 0)
+                addChild(thumb)
+            }
         }
-        
-        self.init(color: clip.clip.content.color, size: CGSize(width: toViewScale.apply(clip.clip.length), height: height))
-        
-        let aspectRatio: CGFloat = 16 / 9
-        let thumbSize = CGSize(width: aspectRatio * height, height: height)
-        let thumb = generateThumbnail(from: clip.clip, size: thumbSize)
-        thumb.centerLeftPosition = CGPoint(x: frame.minX, y: 0)
-        addChild(thumb)
-        
-        centerLeftPosition = CGPoint(x: toClipX.apply(clip.offset), y: 0)
     }
 }

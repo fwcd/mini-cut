094d5ed28cb187bf3970678866bb0654ad325c8a
fwcd <fwcdmail@gmail.com>
2021-04-01 23:05:24 +0200

    Remove demo video from VideoView

--- MiniCut.playground/Sources/View/VideoView.swift
+++ MiniCut.playground/Sources/View/VideoView.swift
@@ -6,41 +6,28 @@ private let cursorActionKey = "cursorAction"
 private let cursorStride: TimeInterval = 0.1
 
 /// A view of the composited video.
-final class VideoView: SKNode {
+final class VideoView: SKSpriteNode {
     private var state: MiniCutState!
     private var isPlayingSubscription: Subscription!
     private var cursorSubscription: Subscription!
     private var updateStartSubscription: Subscription!
     
-    private var size: CGSize!
     private var crop: SKCropNode!
     
     private var startDate: Date?
     private var startCursor: TimeInterval?
     
     convenience init(state: MiniCutState, size: CGSize) {
-        self.init()
+        self.init(color: .black, size: size)
         self.state = state
-        self.size = size
         
         crop = SKCropNode()
         crop.maskNode = SKSpriteNode(color: .white, size: size)
         addChild(crop)
         
-        // TODO: Remove this sample and implement an actual composited video preview
-        let url = Bundle.main.url(forResource: "bigBuckBunny", withExtension: "mp4")!
-        let player = AVPlayer(url: url)
-        let video = SKVideoNode(avPlayer: player)
-        video.size = size
-        video.zPosition = 100
-        crop.addChild(video)
-        
         updateStartSubscription = state.cursorWillChange.subscribeFiring(state.cursor) { [unowned self] in
             startDate = Date()
             startCursor = $0
-        }
-        cursorSubscription = state.cursorWillChange.subscribeFiring(state.cursor) {
-            player.seek(to: CMTime(seconds: $0, preferredTimescale: 1_000))
         }
         isPlayingSubscription = state.isPlayingWillChange.subscribeFiring(state.isPlaying) { [unowned self] in
             if $0 {
@@ -49,18 +36,14 @@ final class VideoView: SKNode {
                 
                 run(.repeatForever(.sequence([
                     .run {
-                        state.cursorWillChange.silencing([cursorSubscription, updateStartSubscription]) {
+                        state.cursorWillChange.silencing(updateStartSubscription) {
                             state.cursor = startCursor! - startDate!.timeIntervalSinceNow
                         }
                     },
                     .wait(forDuration: cursorStride)
                 ])), withKey: cursorActionKey)
-                
-                player.play()
             } else {
                 removeAction(forKey: cursorActionKey)
-                
-                player.pause()
             }
         }
     }

b01ef4b9b16700bd4eb3a4c5c3575068be589c51
fwcd <fwcdmail@gmail.com>
2021-04-01 22:10:49 +0200

    Set position inside TrackClipView

--- MiniCut.playground/Sources/View/TrackClipView.swift
+++ MiniCut.playground/Sources/View/TrackClipView.swift
@@ -3,7 +3,14 @@ import SpriteKit
 
 /// A visual representation of a track's controls.
 final class TrackClipView: SKSpriteNode {
-    convenience init(state: MiniCutState, trackId: UUID, id: UUID, height: CGFloat, toViewScale: AnyBijection<TimeInterval, CGFloat>) {
+    convenience init(
+        state: MiniCutState,
+        trackId: UUID,
+        id: UUID,
+        height: CGFloat,
+        toViewScale: AnyBijection<TimeInterval, CGFloat>,
+        toClipX: AnyBijection<TimeInterval, CGFloat>
+    ) {
         // TODO: Subscribe to listeners
         guard let clip = state.timeline[trackId]?[id] else {
             fatalError("Could not find track with id \(trackId)")
@@ -16,5 +23,7 @@ final class TrackClipView: SKSpriteNode {
         let thumb = generateThumbnail(from: clip.clip, size: thumbSize)
         thumb.centerLeftPosition = CGPoint(x: frame.minX, y: 0)
         addChild(thumb)
+        
+        centerLeftPosition = CGPoint(x: toClipX.apply(clip.offset), y: 0)
     }
 }
--- MiniCut.playground/Sources/View/TrackView.swift
+++ MiniCut.playground/Sources/View/TrackView.swift
@@ -7,7 +7,13 @@ final class TrackView: SKSpriteNode {
     
     private(set) var clipNodes: [UUID: SKNode] = [:]
     
-    convenience init(state: MiniCutState, id: UUID, size: CGSize, marked: Bool, toViewScale: AnyBijection<TimeInterval, CGFloat>) {
+    convenience init(
+        state: MiniCutState,
+        id: UUID,
+        size: CGSize,
+        marked: Bool,
+        toViewScale: AnyBijection<TimeInterval, CGFloat>
+    ) {
         self.init(color: marked ? ViewDefaults.quaternary : ViewDefaults.transparent, size: size)
         
         let track = state.timeline[id] ?? Track(id: id, name: "<undefined>")
@@ -19,14 +25,12 @@ final class TrackView: SKSpriteNode {
         let clips = SKNode()
         addChild(clips)
         
-        let toClipPos = toViewScale + (trackControlsWidth - (size.width / 2))
+        let toClipX = (toViewScale + (trackControlsWidth - (size.width / 2))).erase()
         
         clipsSubscription = state.timelineDidChange.subscribeFiring(state.timeline) { [unowned self] in
             guard let track = $0[id] else { return }
             clips.diffUpdate(nodes: &clipNodes, with: track.clips) {
-                let clip = TrackClipView(state: state, trackId: id, id: $0.id, height: size.height, toViewScale: toViewScale)
-                clip.centerLeftPosition = CGPoint(x: toClipPos.apply($0.offset), y: 0)
-                return clip
+                TrackClipView(state: state, trackId: id, id: $0.id, height: size.height, toViewScale: toViewScale, toClipX: toClipX)
             }
         }
     }

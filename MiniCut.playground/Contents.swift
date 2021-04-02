6e9418dd56ebb6fba5d9a86d1face75dc2b38e18
fwcd <fwcdmail@gmail.com>
2021-04-02 18:18:19 +0200

    Update playground size

--- MiniCut.playground/Contents.swift
+++ MiniCut.playground/Contents.swift
@@ -8,8 +8,8 @@
 import PlaygroundSupport
 import SpriteKit
 
-let width = 900
-let height = 700
+let width = 1050
+let height = 600
 
 // Create and initialize the scene
 let sceneView = SKView(frame: CGRect(x: 0, y: 0, width: width, height: height))

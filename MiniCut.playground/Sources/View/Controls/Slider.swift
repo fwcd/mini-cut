d28a52c8ce82c653033ed93e056c0cb191d20008
fwcd <fwcdmail@gmail.com>
2021-04-02 23:54:04 +0200

    Implement slider logic

--- MiniCut.playground/Sources/View/Controls/Slider.swift
+++ MiniCut.playground/Sources/View/Controls/Slider.swift
@@ -3,12 +3,14 @@ import SpriteKit
 
 /// A simple UI control that lets the user select a numeric value from a range.
 public class Slider: SKNode, SKInputHandler {
+    private var size: CGSize!
     private var knob: SKShapeNode!
     
     private var knobInactiveBgColor: Color!
     private var knobActiveBgColor: Color!
-    private var padding: CGFloat!
-    private var action: ((CGFloat) -> Void)?
+    private var action: ((Double) -> Void)?
+    
+    private var toViewX: AnyBijection<Double, CGFloat>!
     
     public override var isUserInteractionEnabled: Bool {
         get { true }
@@ -16,6 +18,7 @@ public class Slider: SKNode, SKInputHandler {
     }
     
     public convenience init(
+        range: Range<Double>,
         size: CGSize,
         trackThickness: CGFloat = ViewDefaults.sliderTrackThickness,
         trackBgColor: Color = ViewDefaults.inactiveBgColor,
@@ -23,13 +26,20 @@ public class Slider: SKNode, SKInputHandler {
         knobInactiveBgColor: Color = ViewDefaults.knobInactiveBgColor,
         knobActiveBgColor: Color = ViewDefaults.knobActiveBgColor,
         sliderKnobRadius
-        action: ((CGFloat) -> Void)? = nil
+        action: ((Double) -> Void)? = nil
     ) {
         self.init()
+        self.size = size
         self.knobInactiveBgColor = knobInactiveBgColor
         self.knobActiveBgColor = knobActiveBgColor
-        self.padding = padding
         self.action = action
+        
+        toViewX = InverseTranslation(offset: range.lowerBound)
+            .then(InverseScaling(divisor: range.upperBound - range.lowerBound))
+            .then(AnyBijection(CGFloat.init(_:), Double.init(_:)))
+            .then(Scaling(factor: size.width))
+            .then(InverseTranslation(offset: size.width / 2))
+            .erase()
         
         let track = SKSpriteNode(color: trackBgColor, size: CGSize(width: size.width, height: trackThickness))
         addChild(track)
@@ -40,23 +50,18 @@ public class Slider: SKNode, SKInputHandler {
         addChild(knob)
     }
     
-    private func active(at point: CGPoint) -> Bool {
-        parent.map { contains(convert(point, to: $0)) } ?? false
-    }
-    
     public func inputDown(at point: CGPoint) {
         knob.fillColor = knobActiveBgColor
     }
     
     public func inputDragged(to point: CGPoint) {
-        knob.fillColor = active(at: point) ? knobActiveBgColor : knobInactiveBgColor
+        let newX = max(min(point.x, size.width / 2), -(size.width / 2))
+        knob.position = CGPoint(x: newX, y: 0)
+        action?(toViewX.inverseApply(newX))
     }
     
     public func inputUp(at point: CGPoint) {
+        inputDragged(to: point)
         knob.fillColor = knobInactiveBgColor
-        if active(at: point) {
-//            action?(self)
-            // TODO
-        }
     }
 }
--- MiniCut.playground/Sources/View/InspectorView.swift
+++ MiniCut.playground/Sources/View/InspectorView.swift
@@ -16,7 +16,7 @@ final class InspectorView: SKSpriteNode {
         let content = SKNode()
         let contentSize = CGSize(width: size.width, height: size.height - (2 * ViewDefaults.padding) - headerFrame.height)
         addChild(content)
-        addChild(Slider(size: size) { _ in /* todo */ })
+        addChild(Slider(range: -2..<2, size: size) { _ in /* todo */ })
         
         selectionSubscription = state.selectionDidChange.subscribeFiring(state.selection) {
             content.removeAllChildren()

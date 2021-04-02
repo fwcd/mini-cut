2a96c03d701a47fb8640bfe594f983c9a1f7a7bc
fwcd <fwcdmail@gmail.com>
2021-04-02 20:26:54 +0200

    Implement (WIP) inspector for text clips

--- MiniCut.playground/Sources/View/InspectorClipView.swift
+++ MiniCut.playground/Sources/View/InspectorClipView.swift
@@ -3,6 +3,20 @@ import SpriteKit
 
 /// A property inspector view for a single clip.
 final class InspectorClipView: SKNode {
+    private var state: MiniCutState!
+    private var selectionSubscriptions: [Subscription] = []
+    private var trackId: UUID!
+    private var clipId: UUID!
+    
+    private var clip: OffsetClip? {
+        get { state.timeline[trackId]?[clipId] }
+        set { state.timeline[trackId]?[clipId] = newValue }
+    }
+    private var content: ClipContent? {
+        get { clip?.clip.content }
+        set { clip?.clip.content = newValue! }
+    }
+    
     convenience init(
         state: MiniCutState,
         textFieldSelection: TextFieldSelectionController,
@@ -11,18 +25,26 @@ final class InspectorClipView: SKNode {
         size: CGSize
     ) {
         self.init()
+        self.state = state
+        self.trackId = trackId
+        self.clipId = clipId
         
-        let stack = Stack.vertical(anchored: true, [Label("TODO", fontSize: ViewDefaults.headerFontSize, fontColor: ViewDefaults.secondary)])
-        stack.position = CGPoint(x: 0, y: (size.height / 2) - ViewDefaults.padding - (stack.calculateAccumulatedFrame().height / 2))
-        addChild(stack)
-        
-        
-        
-//        // TODO
-//        let textField = TextField(size: CGSize(width: size.width, height: ViewDefaults.textFieldHeight), text: "Test") { _ in
-//            // TODO
-//        }
-//        stack.addChild(textField)
-//        textFieldSubscriptions.append(textFieldSelection.register(textField: textField))
+        switch content {
+        case .text(let text):
+            let textField = TextField(size: CGSize(width: size.width * 0.8, height: ViewDefaults.textFieldHeight), text: text.text) { [unowned self] in
+                guard case .text(var newText) = content else { return }
+                newText.text = $0
+                content = .text(newText)
+            }
+            selectionSubscriptions.append(textFieldSelection.register(textField: textField))
+            addChild(Stack.vertical([
+                Stack.horizontal([
+                    Label("Text:"),
+                    textField
+                ])
+            ]))
+        default:
+            break
+        }
     }
 }

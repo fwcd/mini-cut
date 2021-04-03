aed270d4186870f00acbcfedf5e556e6fd792b71
fwcd <fwcdmail@gmail.com>
2021-04-03 20:32:08 +0200

    Implement clip imports

--- MiniCut.playground/Sources/View/LibraryClipsView.swift
+++ MiniCut.playground/Sources/View/LibraryClipsView.swift
@@ -1,4 +1,5 @@
 import Foundation
+import AVFoundation
 import SpriteKit
 
 /// A view of the user's clip library.
@@ -14,6 +15,9 @@ final class LibraryClipsView: SKNode {
     ) {
         super.init()
         
+        let clipsWrapper = SKNode()
+        addChild(clipsWrapper)
+        
         librarySubscription = state.libraryWillChange.subscribeFiring(state.library) { [unowned self] in
             let clips = Flow(size: CGSize(width: size.width - (padding * 2), height: size.height - (padding * 2)))
             clips.position = CGPoint(x: -(size.width / 2) + padding, y: (size.height / 2) - padding)
@@ -25,12 +29,14 @@ final class LibraryClipsView: SKNode {
                 dndSubscriptions.append(dragNDrop.register(source: clip))
             }
             
-            removeAllChildren()
-            addChild(clips)
+            clipsWrapper.removeAllChildren()
+            clipsWrapper.addChild(clips)
         }
         
         let importButton = Button("Import Clips...", height: ViewDefaults.smallButtonSize, fontSize: ViewDefaults.smallButtonSize) { [unowned self] _ in
-            runFilePicker { _ in } // TODO
+            runFilePicker { urls in
+                state.library.clips += urls.map(Clip.init(url:))
+            }
         }
         importButton.bottomCenterPosition = CGPoint(x: 0, y: -(size.height / 2) + padding)
         addChild(importButton)
